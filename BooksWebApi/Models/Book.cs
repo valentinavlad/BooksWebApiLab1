53bb243a9e33238e148ed81375ab1d5ccdb316ba
Valentina Vlad <vlad.valentina.diana@gmail.com>
2020-05-11 12:42:30 +0300

    Add project files.

---
+++ BooksWebApi/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksWebApi.Models
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Title 
+        { 
+            get 
+            { 
+                return Title; 
+            }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Title must not be blank");
+                Title = value;
+            }
+        }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
